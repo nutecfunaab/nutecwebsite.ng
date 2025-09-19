9172d7e82064a587b6fb0733f4603d3a2c833c85
nutecfunaab <nutecfunaab@gmail.com>
2025-09-19 15:52:10 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
+import React from "react"; // <-- make sure React is imported
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./App.tsx";
-  import "./index.css";
-
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
